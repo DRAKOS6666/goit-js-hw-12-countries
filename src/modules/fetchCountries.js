f2bd0bda0c3f4010fe99e60944eccbe8e9249317
DRAKOS <darkostius2@gmail.com>
2021-11-15 12:03:35 +0200

    api is changed

--- src/modules/fetchCountries.js
+++ src/modules/fetchCountries.js
@@ -1,6 +1,6 @@
 function fetchCountries(searchQuery) {
     return fetch(`
-    https://restcountries.eu/rest/v2/name/${searchQuery}`).then(res => res.json());
+    http://api.countrylayer.com/v2/name/${searchQuery}?access_key=231611225b9d41928ad33480ce9fc4bb`).then(res => res.json());
 }
 
 export default fetchCountries;
